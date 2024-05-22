e9dc31bf6e1611eff42cb2b14ab319e74478a9c1
tuky191 <michal@terra.money>
2024-05-22 16:27:36 +0200

    move test to utils

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"server/v0/pkg/server"
 	"server/v0/pkg/utils"
-
-	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -15,7 +13,6 @@ func main() {
 	if err != nil {
 		os.Exit(1)
 	}
-	spew.Dump(serverConfig)
 	server, err := server.NewServer(*serverConfig)
 	if err != nil {
 		log.Printf("Unable to start server: %v", err)
